ad5aa93b277d13e638857f4b53e79ed50079410e
hoanganhle225 <hoanganhle225@gmail.com>
2025-03-26 10:29:03 +0700

    first commit

---
+++ src/main/java/com/example/aiagent/ModMain.java
@@ -0,0 +1,10 @@
+package com.example.aiagent;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("aiagent")
+public class ModMain {
+    public ModMain() {
+        System.out.println("[AIAGENT] Mod đã khởi động thành công!");
+    }
+}
